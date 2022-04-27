f7a7ca9376524f7383716bf9b8a2d1153b19e44a
FSANDOVALC <fsandovalc@ucenfotec.ac.cr>
2022-04-26 19:25:28 -0600

    Graphql lab funcional

---
+++ src/main/java/com/cenfotec/graphql/PrjGraphQL/repositories/VehicleRepository.java
@@ -0,0 +1,10 @@
+package com.cenfotec.graphql.PrjGraphQL.repositories;
+
+import com.cenfotec.graphql.PrjGraphQL.entities.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+}
+
